65f683da6fa768ff06344194c24063bc3a6fd8f7
Pedro Henrique <pedrinsantx15@gmail.com>
2026-08-03 14:45:52 -0300

    wip: Layout padrão

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ use App\Http\Controllers\LivroController;
 
 Route::get('/livros', [LivroController::class, 'index']);
 Route::post('/livros', [LivroController::class, 'store']);
+
+use App\Http\Controllers\ProdutoController;
+
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
