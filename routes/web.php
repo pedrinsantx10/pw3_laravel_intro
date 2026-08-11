6d8e6f53c9a63df376e78567b77662b8ae104b6c
Pedro Henrique <pedrinsantx15@gmail.com>
2026-08-11 13:43:36 -0300

    wip: Landing Page com Tailwind

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::view('/landing', 'landing');
 
 use App\Http\Controllers\LivroController;
 
